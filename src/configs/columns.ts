852ce94402e95f30160e820a6477b0d9c7424c28
megamind <echingalo@gmail.com>
2023-09-14 11:46:33 +0300

    refactor: added other columns on the configuration

---
+++ src/configs/columns.ts
@@ -0,0 +1,134 @@
+import { ProgramConfig } from "../types";
+
+export const columnMappings: ProgramConfig = {
+  hOEIHJDrrvz: {
+    attributeColumns: [
+      {
+        attribute: "WTZ7GLTrE8Q",
+        column: "First Name",
+      },
+      {
+        attribute: "s1HaiT6OllL",
+        column: "Middle Name",
+      },
+      {
+        attribute: "rSP9c21JsfC",
+        column: "Surname",
+      },
+      {
+        attribute: "fTSAY18LgCF",
+        column: "Primary UIC",
+      },
+      {
+        attribute: "qZP982qpSPS",
+        column: "Date of Birth",
+      },
+    ],
+    eventColumns: [
+      {
+        column: "ART",
+        dataElement: "uWKbFmG9ZWQ",
+        programStage: "Gk494qKQP8B",
+      },
+      {
+        column: "Family Planning/SRH",
+        dataElement: "uciT2F6ByYO",
+        programStage: "A7Tl3vML6as",
+      },
+      {
+        column: "HIV Risk Assessment",
+        dataElement: "qFwm4RM45gi",
+        programStage: "PGFt6IwdZLM",
+      },
+      {
+        column: "Post GBV (Clinical)",
+        dataElement: "mnYT2rZyGgJ",
+        programStage: "yK0ENCuwPqh",
+      },
+      {
+        column: "ANC",
+        dataElement: "irtbsom4W8P",
+        programStage: "wA5y7RU83lF",
+      },
+      {
+        column: "PEP",
+        dataElement: "mcgHO4djXTu",
+        programStage: "CEyIqiOZOwx",
+      },
+      {
+        column: "Condom Education Provision",
+        dataElement: "d4AJf9yiKpL",
+        programStage: "NXsIkG9Q1BA",
+      },
+      {
+        column: "HTS",
+        dataElement: "zbHpXUjGv5H",
+        programStage: "vAMc8n0YB6m",
+      },
+      {
+        column: "PrEP",
+        dataElement: "VtmkYCQkBQw",
+        programStage: "Qw8c20q5V0w",
+      },
+      {
+        column: "Parenting (Preg & Breastfeeding)",
+        dataElement: "NhUVtfObJFw",
+        programStage: "N5SlNqQuMyC",
+      },
+      {
+        column: "Treatment Support (TB/ARV)",
+        dataElement: "kUG5Q0Xp4Ng",
+        programStage: "uAKMWyJlXEN",
+      },
+      {
+        column: "POST GBV (Legal)",
+        dataElement: "qML4gVZ2UFc",
+        programStage: "VLW93YjZOyf",
+      },
+      {
+        column: "HIV Prevention Education",
+        dataElement: "ytVTXe4RVUJ",
+        programStage: "mCKbTbsQoMy",
+      },
+      {
+        column: "Violence Prevention Education",
+        dataElement: "u91LWo6Lz5E",
+        programStage: "pbav4aU666s",
+      },
+      {
+        column: "Condom Education/Provision",
+        dataElement: "d4AJf9yiKpL",
+        programStage: "NXsIkG9Q1BA",
+      },
+      {
+        column: "Condom Education/Provision",
+        dataElement: "d4AJf9yiKpL",
+        programStage: "NXsIkG9Q1BA",
+      },
+      {
+        column: "PrEP",
+        dataElement: "veoA322323t",
+        programStage: "mMjGlK1W0Xo",
+      },
+      {
+        column: "PrEP",
+        dataElement: "VtmkYCQkBQw",
+        programStage: "Qw8c20q5V0w",
+      },
+      {
+        column: "Facility Referral",
+        dataElement: "MkyTrLeBG8I",
+        programStage: "OrC9Bh2bcFz",
+      },
+      {
+        column: "Community Referral",
+        dataElement: "rsh5Kvx6qAU",
+        programStage: "OrC9Bh2bcFz",
+      },
+      {
+        dataElement: "Eug4BXDFLym-vL6NpUA0rIU",
+        programStage: "bDJq2JWVTbC",
+      },
+    ],
+  },
+};
