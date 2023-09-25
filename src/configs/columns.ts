4ae9d19eb60979e68897ba10effb57ef18401294
megamind <echingalo@gmail.com>
2023-09-25 15:34:49 +0300

    refactor: added configuration for ovc and caregiver programs

--- src/configs/columns.ts
+++ src/configs/columns.ts
@@ -126,4 +126,767 @@ export const columnMappings: ProgramConfig = {
       },
     ],
   },
+  em38qztTI8s: {
+    attributeColumns: [
+      {
+        attribute: "WTZ7GLTrE8Q",
+        column: "BENEFICIARY NAME",
+      },
+      {
+        attribute: "s1HaiT6OllL",
+        column: "BENEFICIARY MIDDLE NAME",
+      },
+      {
+        attribute: "rSP9c21JsfC",
+        column: "BENEFICIARY SURNAME",
+      },
+      {
+        attribute: "vIX4GTSCX4P",
+        column: "SEX",
+      },
+
+      {
+        attribute: "qZP982qpSPS",
+        column: "DATE OF BIRTH",
+      },
+      {
+        attribute: "RB8Wx75hGa4",
+        column: "VILLAGE",
+      },
+      {
+        attribute: "orgUnitName",
+        column: "ENROLLED AT",
+      },
+      {
+        attribute: "enrollmentDate",
+        column: "ASSESSMENT DATE",
+      },
+      {
+        attribute: "RB8Wx75hGa4",
+        column: "VILLAGE",
+      },
+      {
+        attribute: "KO5NC4pfBmv",
+        column: "BENEFICIARY TYPE",
+      },
+      {
+        attribute: "fTSAY18LgCF",
+        column: "PRIMARY UIC",
+      },
+      {
+        attribute: "eIU7KMx4Tu3",
+        column: "BENEFICIARY CODE",
+      },
+      {
+        attribute: "tNdoR0jYr7R",
+        column: "PHONE NUMBERS",
+      },
+      {
+        attribute: "RDobagXItZ6",
+        column: "NEW OR RETURNING",
+      },
+      {
+        attribute: "PN92g65TkVI",
+        column: "BENEFICIARY STATUS",
+      },
+      {
+        attribute: "mTv9eZZq0Nz",
+        column: "PRIMARY VULNERABILITY",
+      },
+      {
+        attribute: "wmKqYZML8GA",
+        column: "CHILD LIVING WITH HIV",
+      },
+      {
+        attribute: "GMcljM7jbNG",
+        column: "HIV EXPOSED INFANTS",
+      },
+      {
+        attribute: "ZKMhrjWoXnD",
+        column: "CHILD OF PLHIV",
+      },
+      {
+        attribute: "Gkjp5XZD70V",
+        column: "CHILD EXPOSED/EXPERIENCING VIOLENCE AND ABUSE",
+      },
+      {
+        attribute: "br1xvwAQ6el",
+        column: "CHILD OF FSW",
+      },
+      {
+        attribute: "UeF4OvjIIEK",
+        column: "ORPHANS(SINGLE & DOUBLE)",
+      },
+      {
+        attribute: "YR7Xxk14qoP",
+        column: "CHILDREN LIVING WITH DISABILITIES",
+      },
+      {
+        attribute: "WsmWkkFBiT6",
+        column: "SIBLINGS OF OTHER SUB-POPULATIONS",
+      },
+      {
+        attribute: "Sa0KVprHUr7",
+        column: "When did the violence/abuse happen",
+      },
+      {
+        attribute: "m26lCJGANwu",
+        column: "Experience sexual violence/abuse",
+      },
+      {
+        attribute: "BGJgzqszT0H",
+        column: "Experience Economic(Neglect) violence/abuse",
+      },
+      {
+        attribute: "WAjYVtFWI2n",
+        column: "Experience Physical violence/abuse",
+      },
+      {
+        attribute: "lm4BA6iOdlI",
+        column: "Experience Emotional violence/abuse",
+      },
+      {
+        attribute: "Mc3k3bSwXNe",
+        column: "Action taken",
+      },
+      {
+        attribute: "OLbwYRoywcZ",
+        column: "Case completed?",
+      },
+    ],
+    eventColumns: [
+      {
+        column: "18-20 IN SCHOOL",
+        dataElement: "Uw2Z1XNFgXI",
+        programStage: "dVdlJNtNLIQ",
+      },
+      {
+        column: "CURRENT HIV STATUS",
+        dataElement: "uRsImCXXDXe",
+        programStage: "v9Vrc5exzam",
+      },
+      {
+        column: "HIV RISK ASSESSMENT RESULT",
+        dataElement: "eSJhbqT1NQb",
+        programStage: "RJTRRO0wg8H",
+      },
+      {
+        column: "CASE PLAN UPDATE DATE",
+        dataElement: "eventDate",
+        programStage: "gkNKXUxpyv9",
+      },
+      {
+        column: "HIVSCREEN Case plan",
+        dataElement: "yJQi4cbzeBF",
+        programStage: "gkNKXUxpyv9",
+      },
+      {
+        column: "HTS Case plan",
+        dataElement: "XoSPWmpWXCy",
+        programStage: "gkNKXUxpyv9",
+      },
+      {
+        column: "HIVTREAT Case plan",
+        dataElement: "ylSjcj6cv42",
+        programStage: "gkNKXUxpyv9",
+      },
+      {
+        column: "HIVS&D Case plan",
+        dataElement: "cx4xBY4jZXM",
+        programStage: "gkNKXUxpyv9",
+      },
+      {
+        column: "HEALTH Case plan",
+        dataElement: "AxtahhoHcXV",
+        programStage: "gkNKXUxpyv9",
+      },
+      {
+        column: "PRG&L Case plan",
+        dataElement: "d2vXxEPeCq7",
+        programStage: "gkNKXUxpyv9",
+      },
+      {
+        column: "SAIDS Case plan",
+        dataElement: "dTFLFtUUeEK",
+        programStage: "gkNKXUxpyv9",
+      },
+      {
+        column: "IMMUNIZE Case plan",
+        dataElement: "x4yAqv4z2Xv",
+        programStage: "gkNKXUxpyv9",
+      },
+      {
+        column: "MALNU Case plan",
+        dataElement: "AM9oJCOHM7f",
+        programStage: "gkNKXUxpyv9",
+      },
+      {
+        column: "STIMULATE Case plan",
+        dataElement: "BJrrrqQqwQO",
+        programStage: "gkNKXUxpyv9",
+      },
+      {
+        column: "CCFLS Case plan",
+        dataElement: "hJUuQ648wVF",
+        programStage: "gkNKXUxpyv9",
+      },
+      {
+        column: "TBSCREEN Case plan",
+        dataElement: "LswSBpMoG5C",
+        programStage: "gkNKXUxpyv9",
+      },
+      {
+        column: "TBREFER Case plan",
+        dataElement: "gCKoJgBFdob",
+        programStage: "gkNKXUxpyv9",
+      },
+      {
+        column: "FOODPREP Case plan",
+        dataElement: "JSQ3xP1NhuN",
+        programStage: "gkNKXUxpyv9",
+      },
+      {
+        column: "FOODPREV Case plan",
+        dataElement: "AwWKp6KmqgY",
+        programStage: "gkNKXUxpyv9",
+      },
+      {
+        column: "FOODSUPP Case plan",
+        dataElement: "uvJV4WGc5ct",
+        programStage: "gkNKXUxpyv9",
+      },
+      {
+        column: "SOAKAGE PIT Case pla",
+        dataElement: "qTf8VSoojJ2",
+        programStage: "gkNKXUxpyv9",
+      },
+      {
+        column: "WASH MESSAGING Case plan",
+        dataElement: "Hvq9oisH7Gt",
+        programStage: "gkNKXUxpyv9",
+      },
+      {
+        column: "TIPPY TAP Case plan",
+        dataElement: "DQxqyncQtwm",
+        programStage: "gkNKXUxpyv9",
+      },
+      {
+        column: "SILC Case plan",
+        dataElement: "e6NV2ZAzFEh",
+        programStage: "gkNKXUxpyv9",
+      },
+      {
+        column: "SGROUP Case plan",
+        dataElement: "yPP7lkomNfK",
+        programStage: "gkNKXUxpyv9",
+      },
+      {
+        column: "FE Case plan",
+        dataElement: "kvF996ugmMl",
+        programStage: "gkNKXUxpyv9",
+      },
+      {
+        column: "K/TPLOT Case plan",
+        dataElement: "WrjNxZBVHCL",
+        programStage: "gkNKXUxpyv9",
+      },
+      {
+        column: "SLION Case plan",
+        dataElement: "uQiyym8SEvd",
+        programStage: "gkNKXUxpyv9",
+      },
+      {
+        column: "RTEEN Case plan",
+        dataElement: "WiPTQhWLVU1",
+        programStage: "gkNKXUxpyv9",
+      },
+      {
+        column: "RKIDS Case plan",
+        dataElement: "neF08iYV9Os",
+        programStage: "gkNKXUxpyv9",
+      },
+      {
+        column: "VAC LEGAL MESSAGING Case plan",
+        dataElement: "AaqeRcyjbyS",
+        programStage: "gkNKXUxpyv9",
+      },
+      {
+        column: "VAC Case plan",
+        dataElement: "aPmPhwm8Zln",
+        programStage: "gkNKXUxpyv9",
+      },
+      {
+        column: "COUNSELLING Case plan",
+        dataElement: "Ccp4Odlh3BE",
+        programStage: "gkNKXUxpyv9",
+      },
+      {
+        column: "SHELTER Case plan",
+        dataElement: "exHDkX1gPiC",
+        programStage: "gkNKXUxpyv9",
+      },
+      {
+        column: "BIRTHCERT Case plan",
+        dataElement: "hS7aONLz2cq",
+        programStage: "gkNKXUxpyv9",
+      },
+      {
+        column: "AFLAS Case plan",
+        dataElement: "Cb8qzfdrg7d",
+        programStage: "gkNKXUxpyv9",
+      },
+      {
+        column: "HWCLUB Case plan",
+        dataElement: "X47zxNAqMv0",
+        programStage: "gkNKXUxpyv9",
+      },
+      {
+        column: "BURSARY Case plan",
+        dataElement: "qraZh4n14S4",
+        programStage: "gkNKXUxpyv9",
+      },
+      {
+        column: "SFEES Case plan",
+        dataElement: "aTKFUztYIeT",
+        programStage: "gkNKXUxpyv9",
+      },
+      {
+        column: "SMATERIAL Case plan",
+        dataElement: "q2N7p3UbvSF",
+        programStage: "gkNKXUxpyv9",
+      },
+      {
+        column: "REENROL Case plan",
+        dataElement: "NcMANzhhphO",
+        programStage: "gkNKXUxpyv9",
+      },
+      {
+        column: "HIVSCREEN Service",
+        dataElement: "BYu8pI5kGXF",
+        programStage: "CHFwighOquA",
+      },
+      {
+        column: "HTS Service",
+        dataElement: "JnqldNamliR",
+        programStage: "CHFwighOquA",
+      },
+      {
+        column: "HIVTREAT Service",
+        dataElement: "YFgrURiwirq",
+        programStage: "CHFwighOquA",
+      },
+      {
+        column: "HIVS&D Service",
+        dataElement: "HzI5X2yHef6",
+        programStage: "CHFwighOquA",
+      },
+      {
+        column: "HEALTH Service",
+        dataElement: "eqhzeRBMftZ",
+        programStage: "CHFwighOquA",
+      },
+      {
+        column: "PRG&L Service",
+        dataElement: "ESIjwQ9S6Ic",
+        programStage: "CHFwighOquA",
+      },
+      {
+        column: "SAIDS Service",
+        dataElement: "r8wPQAog7PJ",
+        programStage: "CHFwighOquA",
+      },
+      {
+        column: "IMMUNIZE Service",
+        dataElement: "UKczyQWCB0L",
+        programStage: "CHFwighOquA",
+      },
+      {
+        column: "MALNU Service",
+        dataElement: "KkqMjxjAR7g",
+        programStage: "CHFwighOquA",
+      },
+      {
+        column: "CCFLS Service",
+        dataElement: "mRoO7kgpgVg",
+        programStage: "CHFwighOquA",
+      },
+      {
+        column: "STIMULATE Service",
+        dataElement: "GNQ3gDA2CTI",
+        programStage: "CHFwighOquA",
+      },
+      {
+        column: "TBSCREEN Service",
+        dataElement: "gSp9bgPW52L",
+        programStage: "CHFwighOquA",
+      },
+      {
+        column: "TBREFER Service",
+        dataElement: "mY7GqOTKtsQ",
+        programStage: "CHFwighOquA",
+      },
+      {
+        column: "FOODPREP Service",
+        dataElement: "zK7kMYpgPQn",
+        programStage: "CHFwighOquA",
+      },
+      {
+        column: "FOODPREV Service",
+        dataElement: "tnspdPfpuXm",
+        programStage: "CHFwighOquA",
+      },
+      {
+        column: "FOODSUPP Service",
+        dataElement: "DoU7AeHDsUs",
+        programStage: "CHFwighOquA",
+      },
+      {
+        column: "SOAKAGE PIT Service",
+        dataElement: "qL9c5r7c6kK",
+        programStage: "CHFwighOquA",
+      },
+      {
+        column: "WASH MESSAGING Service",
+        dataElement: "gV77yUM8NK8",
+        programStage: "CHFwighOquA",
+      },
+      {
+        column: "TIPPY TAP Service",
+        dataElement: "FRWCmEerPic",
+        programStage: "CHFwighOquA",
+      },
+      {
+        column: "SGROUP Service",
+        dataElement: "AnnOS2m6LZK",
+        programStage: "CHFwighOquA",
+      },
+      {
+        column: "K/TPLOT Service",
+        dataElement: "gfKsz88uxtg",
+        programStage: "CHFwighOquA",
+      },
+      {
+        column: "SLION Service",
+        dataElement: "RNKOtUtLT4c",
+        programStage: "CHFwighOquA",
+      },
+      {
+        column: "RTEEN Service",
+        dataElement: "GImA3HB9YK5",
+        programStage: "CHFwighOquA",
+      },
+      {
+        column: "RKIDS Service",
+        dataElement: "Fj2RbXsb8Yj",
+        programStage: "CHFwighOquA",
+      },
+      {
+        column: "VAC LEGAL MESSAGING Service",
+        dataElement: "QDFZ03HbjGj",
+        programStage: "CHFwighOquA",
+      },
+      {
+        column: "VAC Service",
+        dataElement: "rMtSykWZ4HB",
+        programStage: "CHFwighOquA",
+      },
+      {
+        column: "SHELTER Service",
+        dataElement: "JlRnllAlSk0",
+        programStage: "CHFwighOquA",
+      },
+      {
+        column: "BIRTHCERT Service",
+        dataElement: "aGChpBlIzcd",
+        programStage: "CHFwighOquA",
+      },
+      {
+        column: "AFLAS Service",
+        dataElement: "LcIB5EL3IjT",
+        programStage: "CHFwighOquA",
+      },
+      {
+        column: "HWCLUB Service",
+        dataElement: "hg2bpp1HqwU",
+        programStage: "CHFwighOquA",
+      },
+      {
+        column: "BURSARY Service",
+        dataElement: "fvgPnlOD7e9",
+        programStage: "CHFwighOquA",
+      },
+      {
+        column: "SFEES Service",
+        dataElement: "MeMaUvt3O8j",
+        programStage: "CHFwighOquA",
+      },
+      {
+        column: "SMATERIAL Service",
+        dataElement: "WQlWXUmYjI2",
+        programStage: "CHFwighOquA",
+      },
+      {
+        column: "REENROL Service",
+        dataElement: "Zc0M1jsbfyA",
+        programStage: "CHFwighOquA",
+      },
+    ],
+  },
+  BNsDaCclOiu: {
+    attributeColumns: [
+      {
+        attribute: "WTZ7GLTrE8Q",
+        column: "BENEFICIARY NAME",
+      },
+      {
+        attribute: "s1HaiT6OllL",
+        column: "BENEFICIARY MIDDLE NAME",
+      },
+      {
+        attribute: "rSP9c21JsfC",
+        column: "BENEFICIARY SURNAME",
+      },
+      {
+        attribute: "vIX4GTSCX4P",
+        column: "SEX",
+      },
+      {
+        attribute: "qZP982qpSPS",
+        column: "DATE OF BIRTH",
+      },
+      {
+        attribute: "RB8Wx75hGa4",
+        column: "VILLAGE",
+      },
+      {
+        attribute: "orgUnitName",
+        column: "ENROLLED AT",
+      },
+      {
+        attribute: "enrollmentDate",
+        column: "ASSESSMENT DATE",
+      },
+      {
+        attribute: "RB8Wx75hGa4",
+        column: "VILLAGE",
+      },
+      {
+        attribute: "fTSAY18LgCF",
+        column: "PRIMARY UIC",
+      },
+      {
+        attribute: "eIU7KMx4Tu3",
+        column: "BENEFICIARY CODE",
+      },
+      {
+        attribute: "tNdoR0jYr7R",
+        column: "PHONE NUMBERS",
+      },
+      {
+        attribute: "RDobagXItZ6",
+        column: "NEW OR RETURNING",
+      },
+      {
+        attribute: "PN92g65TkVI",
+        column: "BENEFICIARY STATUS",
+      },
+    ],
+    eventColumns: [
+      {
+        column: "CURRENT HIV STATUS",
+        dataElement: "uRsImCXXDXe",
+        programStage: "JQ74V6yd9On",
+      },
+      {
+        column: "HIV RISK ASSESSMENT RESULT",
+        dataElement: "vNeOE9abQBB",
+        programStage: "uEwup8PaVBd",
+      },
+      {
+        column: "HOUSEHOLD CATEGORIZATION",
+        dataElement: "aEJnSplwvsw",
+        programStage: "vjF07cZNST3",
+      },
+      {
+        column: "CASE PLAN UPDATE DATE",
+        dataElement: "eventDate",
+        programStage: "vjF07cZNST3",
+      },
+      {
+        column: "HTS Case plan",
+        dataElement: "XoSPWmpWXCy",
+        programStage: "vjF07cZNST3",
+      },
+      {
+        column: "HIVTREAT Case plan",
+        dataElement: "ylSjcj6cv42",
+        programStage: "vjF07cZNST3",
+      },
+      {
+        column: "HIVS&D Case plan",
+        dataElement: "cx4xBY4jZXM",
+        programStage: "vjF07cZNST3",
+      },
+      {
+        column: "HEALTH Case plan",
+        dataElement: "AxtahhoHcXV",
+        programStage: "vjF07cZNST3",
+      },
+      {
+        column: "PRG&L Case plan",
+        dataElement: "d2vXxEPeCq7",
+        programStage: "vjF07cZNST3",
+      },
+      {
+        column: "SAIDS Case plan",
+        dataElement: "dTFLFtUUeEK",
+        programStage: "vjF07cZNST3",
+      },
+      {
+        column: "SOAKAGE PIT Case plan",
+        dataElement: "qTf8VSoojJ2",
+        programStage: "vjF07cZNST3",
+      },
+      {
+        column: "WASH MESSAGING Case plan",
+        dataElement: "Hvq9oisH7Gt",
+        programStage: "vjF07cZNST3",
+      },
+      {
+        column: "TIPPY TAP Case plan",
+        dataElement: "DQxqyncQtwm",
+        programStage: "vjF07cZNST3",
+      },
+      {
+        column: "SILC Case plan",
+        dataElement: "e6NV2ZAzFEh",
+        programStage: "vjF07cZNST3",
+      },
+      {
+        column: "K/TPLOT Case plan",
+        dataElement: "WrjNxZBVHCL",
+        programStage: "vjF07cZNST3",
+      },
+      {
+        column: "P&FC Case plan",
+        dataElement: "PoxYSqq8Hgz",
+        programStage: "vjF07cZNST3",
+      },
+      {
+        column: "VAC LEGAL MESSAGING Case plan",
+        dataElement: "AaqeRcyjbyS",
+        programStage: "vjF07cZNST3",
+      },
+      {
+        column: "VAC Case plan",
+        dataElement: "aPmPhwm8Zln",
+        programStage: "vjF07cZNST3",
+      },
+      {
+        column: "COUNSELLING Case plan",
+        dataElement: "Ccp4Odlh3BE",
+        programStage: "vjF07cZNST3",
+      },
+      {
+        column: "BIRTHCERT Case plan",
+        dataElement: "hS7aONLz2cq",
+        programStage: "vjF07cZNST3",
+      },
+      {
+        column: "HWCLUB Case plan",
+        dataElement: "X47zxNAqMv0",
+        programStage: "vjF07cZNST3",
+      },
+      {
+        column: "DATE OF LAST SERVICE",
+        dataElement: "eventDate",
+        programStage: "vcaHzmUuYzU",
+      },
+      {
+        column: "HTS Service",
+        dataElement: "JnqldNamliR",
+        programStage: "vcaHzmUuYzU",
+      },
+      {
+        column: "HIVTREAT Service",
+        dataElement: "YFgrURiwirq",
+        programStage: "vcaHzmUuYzU",
+      },
+      {
+        column: "HIVS&D Service",
+        dataElement: "HzI5X2yHef6",
+        programStage: "vcaHzmUuYzU",
+      },
+      {
+        column: "HEALTH Service",
+        dataElement: "eqhzeRBMftZ",
+        programStage: "vcaHzmUuYzU",
+      },
+      {
+        column: "PRG&L Service",
+        dataElement: "ESIjwQ9S6Ic",
+        programStage: "vcaHzmUuYzU",
+      },
+      {
+        column: "SAIDS Service",
+        dataElement: "r8wPQAog7PJ",
+        programStage: "vcaHzmUuYzU",
+      },
+      {
+        column: "SOAKAGE PIT Service",
+        dataElement: "qL9c5r7c6kK",
+        programStage: "vcaHzmUuYzU",
+      },
+      {
+        column: "WASH MESSAGING Service",
+        dataElement: "gV77yUM8NK8",
+        programStage: "vcaHzmUuYzU",
+      },
+      {
+        column: "TIPPY TAP Service",
+        dataElement: "FRWCmEerPic",
+        programStage: "vcaHzmUuYzU",
+      },
+      {
+        column: "SILC Service",
+        dataElement: "xTO562B5g53",
+        programStage: "vcaHzmUuYzU",
+      },
+      {
+        column: "FE Servicre",
+        dataElement: "lpTVK3t1Ahk",
+        programStage: "vcaHzmUuYzU",
+      },
+      {
+        column: "K/TPLOT Service",
+        dataElement: "lEkrLOFmLrH",
+        programStage: "vcaHzmUuYzU",
+      },
+      {
+        column: "P&FC Service",
+        dataElement: "gwwUOs6yt6C",
+        programStage: "vcaHzmUuYzU",
+      },
+      {
+        column: "VAC LEGAL MESSAGING Service",
+        dataElement: "QDFZ03HbjGj",
+        programStage: "vcaHzmUuYzU",
+      },
+      {
+        column: "VAC Service",
+        dataElement: "rMtSykWZ4HB",
+        programStage: "vcaHzmUuYzU",
+      },
+      {
+        column: "BIRTHCERT Service",
+        dataElement: "aGChpBlIzcd",
+        programStage: "vcaHzmUuYzU",
+      },
+      {
+        column: "HWCLUB Service",
+        dataElement: "hg2bpp1HqwU",
+        programStage: "vcaHzmUuYzU",
+      },
+    ],
+  },
 };
